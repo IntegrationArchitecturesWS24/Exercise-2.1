386e5f4a83b44f5cbc99694efaa3af3804ce9d39
Immanuel Sohn <info@immanuelsohn.de>
2024-11-05 12:35:45 +0000

    formatting

--- routes/openhrm.js
+++ routes/openhrm.js
@@ -1,34 +1,39 @@
-import Express from 'express';
-import axios from 'axios';
-import FormData from 'form-data';
+import Express from "express";
+import axios from "axios";
+import FormData from "form-data";
 
 const router = Express.Router();
-
 
 router.get("/", async (req, res) => {
   try {
     const form = new FormData();
 
-    form.append('client_id', 'api_oauth_id');
-    form.append('client_secret', 'oauth_secret');
-    form.append('grant_type', 'password');
-    form.append('username', 'demouser');
-    form.append('password', '*Safb02da42Demo$');
+    form.append("client_id", "api_oauth_id");
+    form.append("client_secret", "oauth_secret");
+    form.append("grant_type", "password");
+    form.append("username", "demouser");
+    form.append("password", "*Safb02da42Demo$");
 
-    const fetchToken = await axios.post('https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/oauth/issueToken', form);
+    const fetchToken = await axios.post(
+      "https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/oauth/issueToken",
+      form
+    );
 
     const token = fetchToken.data.access_token;
 
-    const response = await axios.get('https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/api/v1/employee/search', {
-      headers: {
-        'Authorization': `Bearer ${token}`
+    const response = await axios.get(
+      "https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/api/v1/employee/search",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-    });
+    );
 
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ message: 'Error fetching data' });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ message: "Error fetching data" });
   }
 });
 
--- util/bonuscalculation.js
+++ util/bonuscalculation.js
@@ -1,13 +1,9 @@
 export function bonusCalculation(company, items) {
   var baseBonus = items * 10;
-  if (company.rating === "excellent")
-    return baseBonus * 7;
-  else if (company.rating === "very good")
-    return baseBonus * 5;
-  else if (company.rating === "good")
-    return baseBonus * 3;
-  else
-    return baseBonus;
+  if (company.rating === "excellent") return baseBonus * 7;
+  else if (company.rating === "very good") return baseBonus * 5;
+  else if (company.rating === "good") return baseBonus * 3;
+  else return baseBonus;
 }
 
 export function Company(name, rating) {
